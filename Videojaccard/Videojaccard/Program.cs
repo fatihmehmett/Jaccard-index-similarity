d9e8111997e14154e26e71615ebfd24b83ba31a9
FatihMehmet <33830220+fatihmehmett@users.noreply.github.com>
2021-06-03 14:27:28 +0300

    Jaccard index/similatiry
    Jaccard benzerliği mesafesi c#

---
+++ Videojaccard/Videojaccard/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Videojaccard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. kelimeyi giriniz");
+            string kelime1 = Console.ReadLine();
+            Console.WriteLine("2. kelimeyi giriniz");
+            string kelime2 = Console.ReadLine();
+
+            float kesisim = 0;
+            float birlesim = 0;
+
+            for(int x = 0; x < kelime1.Length; x++)
+            {
+                for(int i = 0; i < kelime2.Length; i++)
+                {
+                    if (kelime1[x] == kelime2[i])
+                    {
+                        kesisim++;
+                    }
+                }
+            }
+
+            birlesim = (kelime1.Length + kelime2.Length) - kesisim;
+
+            float sonuc = kesisim / birlesim;// jaccard index
+
+            Console.WriteLine(1-sonuc);//jaccard mesafesi
+            Console.ReadKey();
+
+        }
+    }
+}
